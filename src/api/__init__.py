820a1f2e8b8e6fc58ed4b37605899cea3529c474
Andrew <gskll@protonmail.com>
2021-04-17 20:39:52 +0200

    refactor: change file structure
    use __init__.py files for packages
    remove relative imports to work with test suite

---
+++ src/api/__init__.py
@@ -0,0 +1,405 @@
+import os
+from flask import Flask, request, jsonify, abort
+from sqlalchemy import exc
+import json
+from flask_cors import CORS
+
+from datetime import datetime
+
+from database import setup_db, Issue, Comment, User
+from auth import AuthError, requires_auth
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+
+    # CORS Headers
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+
+    #----------------------------------------------------------------------------#
+    #
+    # ROUTES
+    #
+    #----------------------------------------------------------------------------#
+
+    #----------------------------------------------------------------------------#
+    #  POST /users
+    #    it should be a public endpoint
+    #    it should create a new row in the Users table
+    #  returns status code 200 and json {"success": True, "user": user} where user is the newly created user
+    #    or appropriate status code indicating reason for failure
+    #----------------------------------------------------------------------------#
+
+    # TODO: secure /users endpoint??
+    # TODO: add separate test suite
+    '''
+  Includes at least one test for expected success and error behavior for each endpoint using the unittest library
+  Includes tests demonstrating role-based access control, at least two per role.
+  '''
+
+    @app.route('/users', methods=['POST'])
+    def post_users():
+        body = request.get_json()
+
+        for field in body.values():
+            if not field:
+                abort(400)
+
+        user_dict = body.get('user')
+        user_id = user_dict.get('user_id')
+
+        if not user_id:
+            print('400: no user_id provided')
+            abort(400)
+
+        user_exists = User.query.get(user_id)
+
+        if user_exists:
+            print('422: user already exists')
+            abort(422)
+
+        # TODO: delete
+        print(user_dict)
+
+        user = User(
+            id=user_id,
+            username=user_dict.get('nickname'),
+            name=user_dict.get('name'),
+            email=user_dict.get('email'),
+            date_joined=user_dict.get('created_at'),
+            last_login=user_dict.get('created_at'),
+            roles=body.get('roles')
+        )
+
+        try:
+            user.insert()
+        except Exception as e:
+            print('POST /users EXCEPTION >>> ', e)
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'user': user.format_short(),
+            })
+
+    #----------------------------------------------------------------------------#
+    #  GET /issues
+    #    it should be a public endpoint
+    #    it should contain only the issue.format_no_comments() data representation
+    #  returns status code 200 and json {"success": True, "issues": issues} where issues is the list of issues
+    #    or appropriate status code indicating reason for failure
+    #----------------------------------------------------------------------------#
+
+    @app.route('/issues', methods=['GET'])
+    def get_issues():
+        issues = Issue.query.order_by(Issue.id).all()
+        issues = [issue.format_no_comments() for issue in issues]
+
+        return jsonify({
+            "success": True,
+            "issues": issues
+        })
+
+    #----------------------------------------------------------------------------#
+    #  GET /issues/<id>
+    #    where <id> is the existing model id
+    #    it should respond with a 404 error if <id> is not found
+    #    it should be a public endpoint
+    #    it should contain only the issue.format_with_comments() data representation
+    #  returns status code 200 and json {"success": True, "issue": issue} where issue is the issue with id of <id>
+    #    or appropriate status code indicating reason for failure
+    #----------------------------------------------------------------------------#
+
+    @app.route('/issues/<id>', methods=['GET'])
+    @requires_auth('get:issue')
+    def get_issue(auth_payload, id):
+        issue = Issue.query.get(id)
+
+        if not issue:
+            print('No issue found')
+            abort(404)
+
+        issue = issue.format_with_comments()
+
+        return jsonify({
+            "success": True,
+            "issue": issue
+        })
+
+    #----------------------------------------------------------------------------#
+    #  POST /issues
+    #    it should create a new row in the issues table
+    #    it should be require the 'post:issues' permission
+    #    it should contain the issue.format_no_comments() data representation
+    #  returns status code 200 and json {"success": True, "issue": issue} where issue is the newly created issue
+    #    or appropriate status code indicating reason for failure
+    #----------------------------------------------------------------------------#
+
+    @app.route('/issues', methods=['POST'])
+    @requires_auth('post:issues')
+    def post_issue(auth_payload):
+        body = request.get_json()
+
+        for field in body.values():
+            if not field:
+                abort(400)
+
+        issue = Issue(
+            title=body.get('title'),
+            text=body.get('text'),
+            created_at=body.get('created_at'),
+            user_id=body.get('user_id')
+        )
+
+        try:
+            issue.insert()
+        except Exception as e:
+            print('POST /issues EXCEPTION >>> ', e)
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'issue': issue.format_no_comments()
+            })
+
+    #----------------------------------------------------------------------------#
+    #  POST /comments
+    #    it should create a new row in the comments table
+    #    it should be require the 'post:comments' permission
+    #    it should contain the comment.format() data representation
+    #  returns status code 200 and json {"success": True, "comment": comment} where comment is the newly created comment
+    #    or appropriate status code indicating reason for failure
+    #----------------------------------------------------------------------------#
+
+    @app.route('/comments', methods=['POST'])
+    @requires_auth('post:comments')
+    def post_comment(auth_payload):
+        body = request.get_json()
+
+        for field in body.values():
+            if not field:
+                abort(400)
+
+        comment = Comment(
+            text=body.get('text'),
+            created_at=datetime.now(),
+            user_id=body.get('user_id'),
+            issue_id=body.get('issue_id')
+        )
+
+        try:
+            comment.insert()
+        except Exception as e:
+            print('POST /comments EXCEPTION >>> ', e)
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'comment': comment.format()
+            })
+
+    #----------------------------------------------------------------------------#
+    # PATCH /issues/<id>
+    #    where < id > is the existing model id
+    #    it should respond with a 404 error if < id > is not found
+    #    it should edit the corresponding row in the issues table
+    #    it should be require the 'patch:issues' permission
+    #    it should contain the issue.format_no_comments() data representation
+    #  returns status code 200 and json {"success": True, "issue": issue} where issue is the edited issue
+    #    or appropriate status code indicating reason for failure
+    #----------------------------------------------------------------------------#
+
+    @app.route('/issues/<id>', methods=['PATCH'])
+    @requires_auth('patch:issues')
+    def patch_issues(auth_payload, id):
+        issue = Issue.query.get(id)
+        body = request.get_json()
+
+        if not issue:
+            abort(404)
+
+        if not body:
+            abort(400)
+
+        if 'title' in body:
+            issue.title = body.get('title')
+
+        if 'text' in body:
+            issue.text = body.get('text')
+
+        if 'open' in body:
+            issue.open = body.get('open')
+
+            if not body.get('open'):
+                issue.closed_at = datetime.now()
+
+        issue.last_modified = datetime.now()
+
+        try:
+            issue.update()
+        except Exception as e:
+            print('PATCH /issues/<id> EXCEPTION >>> ', e)
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'issue': issue.format_no_comments()
+            })
+
+    #----------------------------------------------------------------------------#
+    #  PATCH /comments/<id>
+    #    where <id> is the existing model id
+    #    it should respond with a 404 error if <id> is not found
+    #    it should edit the corresponding row in the comments table
+    #    it should be require the 'patch:comments' permission
+    #    it should contain the comment.format() data representation
+    #  returns status code 200 and json {"success": True, "comment": comment} where comment is the edited comment
+    #    or appropriate status code indicating reason for failure
+    #----------------------------------------------------------------------------#
+
+    @app.route('/comments/<id>', methods=['PATCH'])
+    @requires_auth('patch:comments')
+    def patch_comments(auth_payload, id):
+        comment = Comment.query.get(id)
+        body = request.get_json()
+
+        if not comment:
+            abort(404)
+
+        if not body:
+            abort(400)
+
+        if 'text' in body:
+            comment.text = body.get('text')
+
+        comment.last_modified = datetime.now()
+
+        try:
+            comment.update()
+        except Exception as e:
+            print('PATCH /comments/<id> EXCEPTION >>> ', e)
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'comment': comment.format()
+            })
+
+    #----------------------------------------------------------------------------#
+    #  DELETE /issues/<id>
+    #    where <id> is the existing model id
+    #    it should respond with a 404 error if <id> is not found
+    #    it should delete the corresponding row in the issues table
+    #    it should be require the 'delete:issues' permission
+    #  returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted issue
+    #    or appropriate status code indicating reason for failure
+    #----------------------------------------------------------------------------#
+
+    @app.route('/issues/<id>', methods=['DELETE'])
+    @requires_auth('delete:issues')
+    def delete_issues(auth_payload, id):
+        issue = Issue.query.get(id)
+
+        if not issue:
+            abort(404)
+
+        try:
+            issue.delete()
+        except Exception as e:
+            print('DELETE /issues/<issue_id> EXCEPTION >>> ', e)
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'delete': id
+            })
+
+    #----------------------------------------------------------------------------#
+    #  DELETE /comments/<id>
+    #    where <id> is the existing model id
+    #    it should respond with a 404 error if <id> is not found
+    #    it should delete the corresponding row in the comments table
+    #    it should be require the 'delete:comments' permission
+    #  returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted comment
+    #    or appropriate status code indicating reason for failure
+    #----------------------------------------------------------------------------#
+
+    @app.route('/comments/<id>', methods=['DELETE'])
+    @requires_auth('delete:comments')
+    def delete_comments(auth_payload, id):
+        comment = Comment.query.get(id)
+
+        if not comment:
+            abort(404)
+
+        try:
+            comment.delete()
+        except Exception as e:
+            print('DELETE /comments/<comment_id> EXCEPTION >>> ', e)
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'delete': id
+            })
+
+    #----------------------------------------------------------------------------#
+    # Error Handling
+    #----------------------------------------------------------------------------#
+
+    @app.errorhandler(AuthError)
+    def authorization_failed(AuthError):
+        return jsonify({
+            'success': False,
+            'error': AuthError.status_code,
+            'message': AuthError.error
+        }), AuthError.status_code
+
+    @app.errorhandler(401)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 401,
+            'message': 'unauthorized'
+        }), 401
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'resource not found'
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'bad request'
+        }), 400
+
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/database/__init__.py
@@ -0,0 +1,233 @@
+import os
+import json
+from flask_sqlalchemy.model import Model
+from sqlalchemy import ARRAY, Column, String, Integer, Boolean, Text, DateTime, ForeignKey
+from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+db = SQLAlchemy()
+
+#----------------------------------------------------------------------------#
+# setup_db(app)
+#     binds a flask application and a SQLAlchemy service
+#----------------------------------------------------------------------------#
+
+
+def setup_db(app, database_path=DATABASE_URL):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+#----------------------------------------------------------------------------#
+# Issue
+# a persistant issue entity, extends the base SQLAlchemy Model
+#----------------------------------------------------------------------------#
+
+class Issue(db.Model):
+    __tablename__ = 'issues'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), unique=True, nullable=False)
+    open = Column(Boolean, default=True)
+    text = Column(Text, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    closed_at = Column(DateTime, nullable=True)
+    last_modified = Column(DateTime, nullable=True)
+
+    # Foreign key
+    user_id = Column(String, ForeignKey('users.id'), nullable=False)
+
+    # Relationships
+    comments = db.relationship('Comment', backref='issue')
+
+    def __repr__(self):
+        return f'<Issue #{self.id}: {self.title}>'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    #----------------------------------------------------------------------------#
+    # format_no_comments()
+    #     short form representation of the Issue model
+    #     without related comments
+    #----------------------------------------------------------------------------#
+
+    def format_no_comments(self):
+        created_at = self.created_at
+        closed_at = self.closed_at
+        last_modified = self.last_modified
+
+        if created_at:
+            created_at = created_at.strftime("%m/%d/%Y, %H:%M:%S")
+
+        if closed_at:
+            closed_at = closed_at.strftime("%m/%d/%Y, %H:%M:%S")
+
+        if last_modified:
+            last_modified = last_modified.strftime("%m/%d/%Y, %H:%M:%S")
+
+        return {
+            'id': self.id,
+            'title': self.title,
+            'open': self.open,
+            'text': self.text,
+            'created_at': created_at,
+            'closed_at': closed_at,
+            'last_modified': last_modified,
+            'user': self.user.format_short()
+        }
+
+    #----------------------------------------------------------------------------#
+    # format_with_comments()
+    #     long form representation of the Issue model
+    #     with related comments
+    #----------------------------------------------------------------------------#
+
+    def format_with_comments(self):
+        created_at = self.created_at
+        closed_at = self.closed_at
+        last_modified = self.last_modified
+
+        if created_at:
+            created_at = created_at.strftime("%m/%d/%Y, %H:%M:%S")
+
+        if closed_at:
+            closed_at = closed_at.strftime("%m/%d/%Y, %H:%M:%S")
+
+        if last_modified:
+            last_modified = last_modified.strftime("%m/%d/%Y, %H:%M:%S")
+
+        return {
+            'id': self.id,
+            'title': self.title,
+            'open': self.open,
+            'text': self.text,
+            'created_at': created_at,
+            'closed_at': closed_at,
+            'last_modified': last_modified,
+            'user': self.user.format_short(),
+            'comments': {comment.id: comment.format() for comment in self.comments}
+        }
+
+
+#----------------------------------------------------------------------------#
+# User
+# a persistant user entity, extends the base SQLAlchemy Model
+#----------------------------------------------------------------------------#
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = Column(String, primary_key=True)
+    username = Column(String(100))
+    name = Column(String(200))
+    email = Column(String(320), nullable=False)
+    date_joined = Column(DateTime, nullable=False)
+    last_login = Column(DateTime, nullable=False)
+    roles = Column(ARRAY(String(100)))
+
+    # relationships
+    issues = db.relationship('Issue', backref='user')
+    comments = db.relationship('Comment', backref='user')
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    #----------------------------------------------------------------------------#
+    # format_short()
+    #     short form representation of the User model
+    #----------------------------------------------------------------------------#
+
+    def format_short(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'name': self.name,
+            'roles': self.roles
+        }
+
+    '''
+    TODO other formats: wait for implementation
+    '''
+
+
+#----------------------------------------------------------------------------#
+# Comment
+# a persistant comment entity, extends the base SQLAlchemy Model
+#----------------------------------------------------------------------------#
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = Column(Integer, primary_key=True)
+    text = Column(Text, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    last_modified = Column(DateTime, nullable=True)
+
+    # Foreign keys
+    user_id = Column(String, ForeignKey('users.id'), nullable=False)
+    issue_id = Column(Integer, ForeignKey('issues.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Comment #{self.id} by {self.user} on issue {self.issue_id}>'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    #----------------------------------------------------------------------------#
+    # format()
+    #     representation of the Comment model
+    #----------------------------------------------------------------------------#
+
+    def format(self):
+        created_at = self.created_at
+        last_modified = self.last_modified
+
+        if created_at:
+            created_at = created_at.strftime("%m/%d/%Y, %H:%M:%S")
+
+        if last_modified:
+            last_modified = last_modified.strftime("%m/%d/%Y, %H:%M:%S")
+
+        return {
+            'id': self.id,
+            'text': self.text,
+            'created_at': created_at,
+            'last_modified': last_modified,
+            'user_id': self.user_id,
+            'issue_id': self.issue_id
+        }
